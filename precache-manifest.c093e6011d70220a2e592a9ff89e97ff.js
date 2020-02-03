356828f905794ea610490c70e3ef8fe725342fdb
Ducky07 <Juuso.laakso@turku.fi>
2020-02-03 15:49:58 +0200

    Updates

---
+++ precache-manifest.c093e6011d70220a2e592a9ff89e97ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e20cc44af5d63dc5d4fef2edd80a4d2",
+    "url": "/monster-finder/index.html"
+  },
+  {
+    "revision": "466f31372af882754ee6",
+    "url": "/monster-finder/static/css/main.bed87934.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/monster-finder/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-finder/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "466f31372af882754ee6",
+    "url": "/monster-finder/static/js/main.1205f3c8.chunk.js"
+  },
+  {
+    "revision": "6f7fb3749b3c380990fe",
+    "url": "/monster-finder/static/js/runtime-main.63746e49.js"
+  }
+]);
